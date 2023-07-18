8d38373e04b83acde8856ef66e613b014c919a25
sradc <sidneyradcliffe@sky.com>
2023-07-18 21:27:21 +0100

    trained a tokenizer on the whole of wiki+bookcorpus and...
    ...tokenized the whole corpus, and wrote the bytes
    into a file that can be loaded as a memory mapped numpy array.
    trained tokenizer in ~1 hour.
    tokenized dataset in ~3 hours.
    size of resulting file is 11GB

---
+++ token_writer.py
@@ -0,0 +1,54 @@
+import os
+import tempfile
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+
+DTYPE = "<u2"  # little endian unsigned 16 bit integer. Only have ~40k tokens
+# assumes the tokenizer does not produce negative tokens
+
+
+class TokenWriter:
+    def __init__(self, path: Union[str, Path]):
+        self.path = Path(path)
+        if self.path.exists():
+            assert self.path.stat().st_size == 0, f"File {self.path} already exists"
+        self.file = open(self.path, "ab")
+
+    def write(self, tokens: np.ndarray):
+        assert tokens.dtype == DTYPE
+        assert tokens.ndim == 1
+        self.file.write(tokens.tobytes())
+
+    def close(self):
+        self.file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+
+def load_tokens(path: Union[str, Path]) -> np.ndarray:
+    # Reading a stream of bytes from a file into a memory mapped array
+    file_size = os.path.getsize(path)
+    num_elements = file_size // np.dtype(DTYPE).itemsize
+    mmapped_array = np.memmap(path, dtype=DTYPE, mode="r", shape=(num_elements,))
+    return mmapped_array
+
+
+if __name__ == "__main__":
+    # Informal test
+    with tempfile.NamedTemporaryFile() as tmp:
+        with TokenWriter(tmp.name) as writer:
+            writer.write(np.arange(10, dtype=DTYPE))
+            writer.write(np.arange(10, dtype=DTYPE) + 10)
+            writer.write(np.arange(10, dtype=DTYPE) + 20)
+        # Test can load
+        tokens = load_tokens(tmp.name)
+        assert np.all(tokens == np.arange(30, dtype=DTYPE))
+        print("Successfully streamed tokens to file and loaded them")
+        print("tokens:")
+        print(tokens)
